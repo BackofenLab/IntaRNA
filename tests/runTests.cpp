7d8dda9e413dbc5c5fff8b0b25be3de874b6c3b6
martin-mann <qyu@gmx.de>
2016-09-29 15:51:53 +0200

    + unit tests added using Catch header library    * configure.ac :      + generate tests/Makefile      + AM_EXTRA_RECURSIVE_TARGETS([tests]) : recursive "tests" make target      * AM_INIT_AUTOMAKE([1.13]) : need v1.13 for AM_EXTRA_RECURSIVE_TARGETS    * Makefile.am :      + go to subdir 'tests' for 'tests' make directive only    + tests/Makefile.am : compile and run tests    + catch.hpp : Catch library version 1.5.7    + tests/runTests.cpp : central test program    + tests/testInteractionEnergy.cpp    + tests/testInteractionEnergyBasePair.cpp    + tests/testRnaSequence.cpp

---
+++ tests/runTests.cpp
@@ -0,0 +1,8 @@
+
+// make this the main test runner
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+
+// don't do anything else, its done via the compiler..
+
---
+++ tests/testInteractionEnergyBasePair.cpp
@@ -0,0 +1,71 @@
+
+#include "catch.hpp"
+
+#undef NDEBUG
+
+#include "InteractionEnergyBasePair.h"
+#include "AccessibilityDisabled.h"
+
+TEST_CASE( "InteractionEnergyBasePair - data access", "[InteractionEnergyBasePair]" ) {
+
+	RnaSequence rna("test","ACGU");
+
+	AccessibilityDisabled acc(rna);
+	ReverseAccessibility rAcc(acc);
+
+	size_t maxLoop1 = 1, maxLoop2 = 2;
+	InteractionEnergyBasePair energy( acc, rAcc, maxLoop1, maxLoop2);
+
+	// check
+	REQUIRE( &energy.getAccessibility1() == &acc );
+	REQUIRE( &energy.getAccessibility2().getAccessibilityOrigin() == &acc );
+	REQUIRE( energy.getMaxInternalLoopSize1() == maxLoop1 );
+	REQUIRE( energy.getMaxInternalLoopSize2() == maxLoop2 );
+
+	REQUIRE( energy.getAccessibility1().getSequence().asString().at(0) == 'A' );
+	REQUIRE( energy.getAccessibility2().getSequence().asString().at(0) == 'U' );
+
+}
+
+TEST_CASE( "InteractionEnergyBasePair - energy computation", "[InteractionEnergyBasePair]" ) {
+
+	RnaSequence rna("test","AGCU");
+
+	AccessibilityDisabled acc(rna);
+	ReverseAccessibility rAcc(acc);
+
+	size_t maxLoop1 = 1, maxLoop2 = 1;
+	InteractionEnergyBasePair energy( acc, rAcc, maxLoop1, maxLoop2);
+
+	// dangling end check
+	REQUIRE( energy.getDanglingLeft(0,0) == 0.0 );
+	REQUIRE( energy.getDanglingLeft(2,2) == 0.0 );
+	REQUIRE( energy.getDanglingRight(0,0) == 0.0 );
+	REQUIRE( energy.getDanglingRight(2,2) == 0.0 );
+
+	// dangling end check
+	REQUIRE( energy.getDanglingLeft(0,0) == 0.0 );
+	REQUIRE( energy.getDanglingLeft(2,2) == 0.0 );
+	REQUIRE( energy.getDanglingRight(0,0) == 0.0 );
+	REQUIRE( energy.getDanglingRight(2,2) == 0.0 );
+
+	// base pairs possible
+	REQUIRE( energy.getInterLoopE( 0,0, 0,0 ) < 0.0 );
+	REQUIRE( energy.getInterLoopE( 0,1, 0,1 ) < 0.0 );
+
+	// base pairs overlapping
+	REQUIRE_FALSE( energy.getInterLoopE( 0,0, 0,1 ) < 0.0 );
+	REQUIRE_FALSE( energy.getInterLoopE( 0,1, 0,0 ) < 0.0 );
+
+	// base pairs not possible
+	REQUIRE_FALSE( energy.getInterLoopE( 0,0, 1,1 ) < 0.0 );
+	REQUIRE_FALSE( energy.getInterLoopE( 0,1, 2,3 ) < 0.0 );
+	REQUIRE_FALSE( energy.getInterLoopE( 0,1, 0,2 ) < 0.0 );
+	REQUIRE_FALSE( energy.getInterLoopE( 0,2, 1,2 ) < 0.0 );
+
+	// loop size exceeded
+	REQUIRE_FALSE( energy.getInterLoopE( 0,3, 1,2 ) < 0.0 );
+	REQUIRE_FALSE( energy.getInterLoopE( 1,2, 0,3 ) < 0.0 );
+	REQUIRE_FALSE( energy.getInterLoopE( 0,3, 0,3 ) < 0.0 );
+
+}
