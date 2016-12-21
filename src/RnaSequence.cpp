c2c53c6bfd2a679cc07cc025c8a80f8e1b089e91
martin-mann <qyu@gmx.de>
2016-12-21 13:32:11 +0100

    support for 'T' in sequences

--- src/RnaSequence.cpp
+++ src/RnaSequence.cpp
@@ -9,61 +9,18 @@
 #include <stdexcept>
 
 
-extern "C" {
-	#include "ViennaRNA/pair_mat.h"
-}
-
 
 
 // setup static members
 std::locale RnaSequence::codeLocale = std::locale();
 
 // setup allowed alphabet in lower and upper case
-const std::string RnaSequence::SequenceAlphabet	= "aAuUcCgGnN";
+const std::string RnaSequence::SequenceAlphabet	= "aAuUcCgGnNtT";
 
 
 ////////////////////////////////////////////////////////////////////////////
 
 const size_t RnaSequence::lastPos = std::string::npos;
-
-/////////////////////////////////////////////////////////////////////////////
-
-RnaSequence::
-Code_type
-RnaSequence::
-getCodeForChar( const char nucleotide )
-{
-	// check if nucleotide character is NOT supported
-	if (SequenceAlphabet.find(nucleotide) == std::string::npos)
-	{
-		// raise exception
-		throw std::runtime_error("RnaSequence::getCodeForChar() : unsupported nucleotide character '"+toString(nucleotide)+"' in sequence");
-	}
-	// otherwise get encoding:
-	// use nucleotide character encoding from Vienna RNA package
-	return (Code_type)encode_char(nucleotide);
-}
-
-/////////////////////////////////////////////////////////////////////////////
-
-bool
-RnaSequence::
-areComplementary( const RnaSequence & s1, const RnaSequence & s2,
-					const size_t p1, const size_t p2 )
-{
-	// check if valid positions
-	if (p1<s1.size() && p2<s2.size()) {
-		// check via VRNA util
-		return BP_pair[s1.seqCode.at(p1)][s2.seqCode.at(p2)] > 0;
-	} else {
-		throw std::runtime_error("RnaSequence::areComplementary : index positions p1/p2 ("
-				+ toString(p1)+"/"+toString(p2)
-				+ ") are out of bounds s1/s2 ("
-				+ toString(s1.size())+"/"+toString(s2.size())
-				+")"
-				);
-	}
-}
 
 /////////////////////////////////////////////////////////////////////////////
 
--- src/RnaSequence.h
+++ src/RnaSequence.h
@@ -1,9 +1,3 @@
-/*
- * RnaSequence.h
- *
- *  Created on: 24.06.2014
- *      Author: Mmann
- */
 
 #ifndef RNASEQUENCE_H_
 #define RNASEQUENCE_H_
@@ -14,6 +8,13 @@
 
 #include "general.h"
 
+
+#ifndef VIENNA_RNA_PAIR_MAT_H
+#define VIENNA_RNA_PAIR_MAT_H
+extern "C" {
+	#include "ViennaRNA/pair_mat.h"
+}
+#endif
 
 
 
@@ -326,11 +327,17 @@ getUpperCase( const std::string & seqString )
 	// create container to fill
 	String_type seqRet(seqString.size(),'_');
 
+//	bool wasDNA = false;
 	for (size_t i=0; i<seqString.size(); ++i)
 	{
 		// get upper case characters
 		seqRet[i] = std::toupper(seqString.at(i),codeLocale);
+		if (seqRet[i]=='T') {
+			seqRet[i] = 'U';
+//			wasDNA = true;
+		}
 	}
+	// TODO report that it wasDNA if so...
 
 	return seqRet;
 }
@@ -374,6 +381,47 @@ operator<<(std::ostream& out, const RnaSequence& rna)
 	// add ID(SEQUENCE) to stream
 	out <<rna.id <<'(' <<rna.asString() <<')';
 	return out;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+inline
+RnaSequence::
+Code_type
+RnaSequence::
+getCodeForChar( const char nucleotide )
+{
+	// check if nucleotide character is NOT supported
+	if (SequenceAlphabet.find(nucleotide) == std::string::npos)
+	{
+		// raise exception
+		throw std::runtime_error("RnaSequence::getCodeForChar() : unsupported nucleotide character '"+toString(nucleotide)+"' in sequence");
+	}
+	// otherwise get encoding:
+	// use nucleotide character encoding from Vienna RNA package
+	return (Code_type)encode_char(nucleotide);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+inline
+bool
+RnaSequence::
+areComplementary( const RnaSequence & s1, const RnaSequence & s2,
+					const size_t p1, const size_t p2 )
+{
+	// check if valid positions
+	if (p1<s1.size() && p2<s2.size()) {
+		// check via VRNA util
+		return BP_pair[s1.seqCode.at(p1)][s2.seqCode.at(p2)] > 0;
+	} else {
+		throw std::runtime_error("RnaSequence::areComplementary : index positions p1/p2 ("
+				+ toString(p1)+"/"+toString(p2)
+				+ ") are out of bounds s1/s2 ("
+				+ toString(s1.size())+"/"+toString(s2.size())
+				+")"
+				);
+	}
 }
 
 /////////////////////////////////////////////////////////////////////////////
