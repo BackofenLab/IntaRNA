0754a9eef87cd718f7f04e3f790216bfdb1db19d
Mostafa Mahmoud <mostafa.amin93@gmail.com>
2017-04-04 22:18:29 +0200

    Fixed tests and few documentations

--- src/IntaRNA/AccessibilityBasePair.h
+++ src/IntaRNA/AccessibilityBasePair.h
@@ -23,11 +23,13 @@ typedef boost::numeric::ublas::matrix<E_type> E2dMatrix;  // Energy matrix
 
   /***
    * Constructor of AccessibilityBasePair
-	 * @param sequence the sequence the accessibility data belongs to
+	 * @param seq The sequence the accessibility data belongs to
 	 * @param maxLength the maximal length of accessible regions (>0) to be
 	 *          considered. 0 defaults to the full sequence's length, otherwise
 	 *          is is internally set to min(maxLength,seq.length).
 	 * @param accConstr optional accessibility constraint
+   * @param basePairEnergy The energy value of the base pairs
+   * @param RT The temperature energy constant
    */
   AccessibilityBasePair(const RnaSequence& seq, const size_t maxLength,
       const AccessibilityConstraint * const accConstr,
@@ -55,7 +57,7 @@ private:
   const E_type basePairEnergy;
   const E_type RT;
   const E_type boltzmann = std::exp(-basePairEnergy / RT);
-  const size_t minLoopLength = 1;
+  const size_t minLoopLength = 3;
   const size_t N;
 
   /***
--- tests/AccessibilityBasePair_test.cpp
+++ tests/AccessibilityBasePair_test.cpp
@@ -5,11 +5,11 @@
 
 #include <boost/numeric/ublas/io.hpp>
 #include "IntaRNA/AccessibilityBasePair.h"
-#include <iostream>
 
-const std::string seq = "gguccac";
+using namespace IntaRNA;
 
-#include <sstream>
+const std::string seq = "gguccacguccaa";
+
 
 TEST_CASE("AccessibilityBasePair", "[AccessibilityBasePair]") {
 
@@ -18,11 +18,18 @@ TEST_CASE("AccessibilityBasePair", "[AccessibilityBasePair]") {
   SECTION("Basepair Energy difference") {
     AccessibilityBasePair acc(rna, 10, NULL);
 
-    REQUIRE( abs( acc.getED(1, 5) - 2.7806 ) < 1e-4 );
-    REQUIRE( abs( acc.getED(3, 4) - 0.8074 ) < 1e-4 );
-    REQUIRE( abs( acc.getED(1, 3) - 2.2256 ) < 1e-4 );
-    REQUIRE( abs( acc.getED(0, 6) - 4.0745 ) < 1e-4 );
-    REQUIRE( abs( acc.getED(6, 6) - 0.3467 ) < 1e-4 );
+    REQUIRE( abs( acc.getED(0, 6) - 4.50986 ) < 1e-4 );
+    REQUIRE( abs( acc.getED(0, 12) - 5.809142) < 1e-4 );
+    REQUIRE( abs( acc.getED(1, 3) - 2.37515 ) < 1e-4 );
+    REQUIRE( abs( acc.getED(1, 5) - 2.74887 ) < 1e-4 );
+    REQUIRE( abs( acc.getED(3, 4) - 0.51919 ) < 1e-4 );
+    REQUIRE( abs( acc.getED(4, 7) - 1.98050 ) < 1e-4 );
+    REQUIRE( abs( acc.getED(4, 12) - 5.809142) < 1e-4 );
+    REQUIRE( abs( acc.getED(5, 5) - 0.0) < 1e-4 );
+    REQUIRE( abs( acc.getED(5, 10) - 3.6119 ) < 1e-4 );
+    REQUIRE( abs( acc.getED(6, 6) - 0.13124 ) < 1e-4 );
+    REQUIRE( abs( acc.getED(10, 11) - 0.56036 ) < 1e-4 );
+    REQUIRE( abs( acc.getED(11, 12) - 0.19116 ) < 1e-4 );
 
   }
 }
