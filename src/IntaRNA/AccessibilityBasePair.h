ab3b95d9adc9fc4e785a6f283b98fa31a1976697
Martin Mann <martin-mann@users.noreply.github.com>
2017-05-03 09:47:19 +0200

    code cleanup

--- src/IntaRNA/AccessibilityBasePair.h
+++ src/IntaRNA/AccessibilityBasePair.h
@@ -8,8 +8,6 @@
 #include "AccessibilityConstraint.h"
 #include "NussinovHandler.h"
 
-#include <boost/numeric/ublas/triangular.hpp>
-
 
 namespace IntaRNA {
 
@@ -19,18 +17,21 @@ class AccessibilityBasePair: public Accessibility {
 public:
   /***
    * Constructor of AccessibilityBasePair
-	 * @param seq The sequence the accessibility data belongs to
-	 * @param maxLength the maximal length of accessible regions (>0) to be
-	 *          considered. 0 defaults to the full sequence's length, otherwise
-	 *          is is internally set to min(maxLength,seq.length).
-	 * @param accConstr optional accessibility constraint
+   * @param seq The sequence the accessibility data belongs to
+   * @param maxLength the maximal length of accessible regions (>0) to be
+   *          considered. 0 defaults to the full sequence's length, otherwise
+   *          is is internally set to min(maxLength,seq.length).
+   * @param accConstr optional accessibility constraint
    * @param basePairEnergy The energy value of the base pairs
    * @param RT The temperature energy constant
    * @param minLoopLength the minimum loop length
    */
-  AccessibilityBasePair(const RnaSequence& seq, const size_t maxLength,
+  AccessibilityBasePair(
+      const RnaSequence& seq, 
+      const size_t maxLength,
       const AccessibilityConstraint * const accConstr,
-      const E_type basePairEnergy = -1, const E_type RT = 1,
+      const E_type basePairEnergy = -1, 
+      const E_type RT = 1,
       const size_t minLoopLength = 3);
 
   /***
@@ -46,7 +47,7 @@ public:
    *
    * @return The ED Value 
    *
-	 * @throw std::runtime_error in case it does not hold 0 <= from <= to < seq.length
+   * @throw std::runtime_error in case it does not hold 0 <= from <= to < seq.length
    */
 	virtual E_type getED( const size_t from, const size_t to ) const;
 
