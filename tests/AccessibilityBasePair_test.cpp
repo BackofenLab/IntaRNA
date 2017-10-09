bf6d7f2ad3b3df12d23606a1993122f9310ad02a
martin-mann <qyu@gmx.de>
2017-10-09 11:39:39 +0200

    explicit use of std::abs

--- tests/AccessibilityBasePair_test.cpp
+++ tests/AccessibilityBasePair_test.cpp
@@ -18,18 +18,18 @@ TEST_CASE("AccessibilityBasePair", "[AccessibilityBasePair]") {
   SECTION("Basepair Energy difference") {
     AccessibilityBasePair acc(rna, 10, NULL);
 
-    REQUIRE( abs( acc.getED(0, 6) - 4.50986 ) < 1e-4 );
-    REQUIRE( abs( acc.getED(0, 12) - 5.809142) < 1e-4 );
-    REQUIRE( abs( acc.getED(1, 3) - 2.37515 ) < 1e-4 );
-    REQUIRE( abs( acc.getED(1, 5) - 2.74887 ) < 1e-4 );
-    REQUIRE( abs( acc.getED(3, 4) - 0.51919 ) < 1e-4 );
-    REQUIRE( abs( acc.getED(4, 7) - 1.98050 ) < 1e-4 );
-    REQUIRE( abs( acc.getED(4, 12) - 5.809142) < 1e-4 );
-    REQUIRE( abs( acc.getED(5, 5) - 0.0) < 1e-4 );
-    REQUIRE( abs( acc.getED(5, 10) - 3.6119 ) < 1e-4 );
-    REQUIRE( abs( acc.getED(6, 6) - 0.13124 ) < 1e-4 );
-    REQUIRE( abs( acc.getED(10, 11) - 0.56036 ) < 1e-4 );
-    REQUIRE( abs( acc.getED(11, 12) - 0.19116 ) < 1e-4 );
+    REQUIRE( std::abs( acc.getED(0, 6) - 4.50986 ) < 1e-4 );
+    REQUIRE( std::abs( acc.getED(0, 12) - 5.809142) < 1e-4 );
+    REQUIRE( std::abs( acc.getED(1, 3) - 2.37515 ) < 1e-4 );
+    REQUIRE( std::abs( acc.getED(1, 5) - 2.74887 ) < 1e-4 );
+    REQUIRE( std::abs( acc.getED(3, 4) - 0.51919 ) < 1e-4 );
+    REQUIRE( std::abs( acc.getED(4, 7) - 1.98050 ) < 1e-4 );
+    REQUIRE( std::abs( acc.getED(4, 12) - 5.809142) < 1e-4 );
+    REQUIRE( std::abs( acc.getED(5, 5) - 0.0) < 1e-4 );
+    REQUIRE( std::abs( acc.getED(5, 10) - 3.6119 ) < 1e-4 );
+    REQUIRE( std::abs( acc.getED(6, 6) - 0.13124 ) < 1e-4 );
+    REQUIRE( std::abs( acc.getED(10, 11) - 0.56036 ) < 1e-4 );
+    REQUIRE( std::abs( acc.getED(11, 12) - 0.19116 ) < 1e-4 );
 
   }
 }
