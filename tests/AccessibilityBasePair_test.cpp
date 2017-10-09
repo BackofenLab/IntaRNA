e63f55875b112e26028226647c5e9e0548a5c928
martin-mann <qyu@gmx.de>
2017-10-09 13:11:45 +0200

    BUGFIX: test values wrong (were derived from rounded values)

--- tests/AccessibilityBasePair_test.cpp
+++ tests/AccessibilityBasePair_test.cpp
@@ -18,18 +18,32 @@ TEST_CASE("AccessibilityBasePair", "[AccessibilityBasePair]") {
   SECTION("Basepair Energy difference") {
     AccessibilityBasePair acc(rna, 10, NULL);
 
-    REQUIRE( std::abs( acc.getED(0, 6) - 4.50986 ) < 1e-4 );
-    REQUIRE( std::abs( acc.getED(0, 12) - 5.809142) < 1e-4 );
-    REQUIRE( std::abs( acc.getED(1, 3) - 2.37515 ) < 1e-4 );
-    REQUIRE( std::abs( acc.getED(1, 5) - 2.74887 ) < 1e-4 );
-    REQUIRE( std::abs( acc.getED(3, 4) - 0.51919 ) < 1e-4 );
-    REQUIRE( std::abs( acc.getED(4, 7) - 1.98050 ) < 1e-4 );
-    REQUIRE( std::abs( acc.getED(4, 12) - 5.809142) < 1e-4 );
+//    std::cout <<"\n\n########\n";
+//    for (int i =0; i<acc.getSequence().size(); i++) {
+//    	std::cout <<(i);
+//    	for (int j=0; j <acc.getSequence().size(); j++) {
+//    		if (j<i) {
+//    			std::cout <<" ---";
+//    		} else {
+//    			std::cout <<" "<<acc.getED(i,j);
+//    		}
+//    	}
+//    	std::cout <<std::endl;
+//    }
+//    std::cout <<"########\n";
+
+    REQUIRE( std::abs( acc.getED(0, 6) - 4.52824 ) < 1e-4 );
+    REQUIRE( std::abs( acc.getED(0, 12) - 5.8415) < 1e-4 );
+    REQUIRE( std::abs( acc.getED(1, 3) - 2.37302 ) < 1e-4 );
+    REQUIRE( std::abs( acc.getED(1, 5) - 2.75135 ) < 1e-4 );
+    REQUIRE( std::abs( acc.getED(3, 4) - 0.519897 ) < 1e-4 );
+    REQUIRE( std::abs( acc.getED(4, 7) - 1.978 ) < 1e-4 );
+    REQUIRE( std::abs( acc.getED(4, 12) - 5.8415) < 1e-4 );
     REQUIRE( std::abs( acc.getED(5, 5) - 0.0) < 1e-4 );
-    REQUIRE( std::abs( acc.getED(5, 10) - 3.6119 ) < 1e-4 );
-    REQUIRE( std::abs( acc.getED(6, 6) - 0.13124 ) < 1e-4 );
-    REQUIRE( std::abs( acc.getED(10, 11) - 0.56036 ) < 1e-4 );
-    REQUIRE( std::abs( acc.getED(11, 12) - 0.19116 ) < 1e-4 );
+    REQUIRE( std::abs( acc.getED(5, 10) - 3.62722 ) < 1e-4 );
+    REQUIRE( std::abs( acc.getED(6, 6) - 0.131359 ) < 1e-4 );
+    REQUIRE( std::abs( acc.getED(10, 11) - 0.559891 ) < 1e-4 );
+    REQUIRE( std::abs( acc.getED(11, 12) - 0.191058 ) < 1e-4 );
 
   }
 }
