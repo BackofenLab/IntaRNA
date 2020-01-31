d2681f6a446bfbc007ab340519c9297aadae1d51
martin-mann <qyu@gmx.de>
2020-01-31 16:20:35 +0100

    + traceback tests (number of bps in traced seeds

--- tests/SeedHandlerMfe_test.cpp
+++ tests/SeedHandlerMfe_test.cpp
@@ -64,6 +64,22 @@ TEST_CASE( "SeedHandlerMfe", "[SeedHandlerMfe]") {
 		/////////////////////////////////////////////   FILLSEED  ////////////////////////////////////////////////
 		//////////////////////////////////////////////////////////////////////////////////////////////////////////
 		REQUIRE(sHM.fillSeed(0,energy.size1()-1, 0,energy.size2()-1) == 4); // getting only one seed per si, thus 2x2
+
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////
+		/////////////////////////////////////////////   TRACEBACK  ///////////////////////////////////////////////
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////
+		size_t si1=9999999, si2=9999999;
+		while( sHM.updateToNextSeed(si1,si2,0,r1.size()-1,0,r2.size()) ) {
+			// prepare trace
+			Interaction i(r1,r2);
+			i.basePairs.push_back(energy.getBasePair(si1,si2));
+			i.basePairs.push_back(energy.getBasePair(si1+sHM.getSeedLength1(si1,si2)-1,si2+sHM.getSeedLength2(si1,si2)-1));
+			// trace base pairs
+			sHM.traceBackSeed(i, si1, si2);
+			// check number of traced base pairs
+			REQUIRE(i.basePairs.size() == 4);
+		}
+
 	}
 
 	SECTION("SeedHandlerMfe: no lp - 1", "[SeedHandlerMfe]") {
@@ -75,42 +91,59 @@ TEST_CASE( "SeedHandlerMfe", "[SeedHandlerMfe]") {
 		InteractionEnergyBasePair energy(acc1, racc);
 
 		{
-		size_t bp = 2;
-		// seedBP / seedMaxUP / seedTMaxUP / seedQMaxUP / seedMaxE / seedMaxED / seedTRange / seedQRange / seedTQ
-		SeedConstraint sC(bp,3,3,0,0
-				, AccessibilityDisabled::ED_UPPER_BOUND
-				, 0
-				, IndexRangeList("")
-				, IndexRangeList("")
-				, ""
-				, false, false, true
-				);
+			size_t bp = 2;
+			// seedBP / seedMaxUP / seedTMaxUP / seedQMaxUP / seedMaxE / seedMaxED / seedTRange / seedQRange / seedTQ
+			SeedConstraint sC(bp,3,3,0,0
+					, AccessibilityDisabled::ED_UPPER_BOUND
+					, 0
+					, IndexRangeList("")
+					, IndexRangeList("")
+					, ""
+					, false, false, true
+					);
 
-		SeedHandlerMfe sHM(energy, sC);
+			SeedHandlerMfe sHM(energy, sC);
 
-		//////////////////////////////////////////////////////////////////////////////////////////////////////////
-		/////////////////////////////////////////////   FILLSEED  ////////////////////////////////////////////////
-		//////////////////////////////////////////////////////////////////////////////////////////////////////////
-		REQUIRE(sHM.fillSeed(0,energy.size1()-1, 0,energy.size2()-1) == 4);
+			//////////////////////////////////////////////////////////////////////////////////////////////////////////
+			/////////////////////////////////////////////   FILLSEED  ////////////////////////////////////////////////
+			//////////////////////////////////////////////////////////////////////////////////////////////////////////
+			REQUIRE(sHM.fillSeed(0,energy.size1()-1, 0,energy.size2()-1) == 4);
 		}
 
 		for (size_t bp = 3; bp <= 4; bp++) {
-		// seedBP / seedMaxUP / seedTMaxUP / seedQMaxUP / seedMaxE / seedMaxED / seedTRange / seedQRange / seedTQ
-		SeedConstraint sC(bp,3,3,0,0
-				, AccessibilityDisabled::ED_UPPER_BOUND
-				, 0
-				, IndexRangeList("")
-				, IndexRangeList("")
-				, ""
-				, false, false, true
-				);
+			// seedBP / seedMaxUP / seedTMaxUP / seedQMaxUP / seedMaxE / seedMaxED / seedTRange / seedQRange / seedTQ
+			SeedConstraint sC(bp,3,3,0,0
+					, AccessibilityDisabled::ED_UPPER_BOUND
+					, 0
+					, IndexRangeList("")
+					, IndexRangeList("")
+					, ""
+					, false, false, true
+					);
 
-		SeedHandlerMfe sHM(energy, sC);
+			SeedHandlerMfe sHM(energy, sC);
 
-		//////////////////////////////////////////////////////////////////////////////////////////////////////////
-		/////////////////////////////////////////////   FILLSEED  ////////////////////////////////////////////////
-		//////////////////////////////////////////////////////////////////////////////////////////////////////////
-		REQUIRE(sHM.fillSeed(0,energy.size1()-1, 0,energy.size2()-1) == 0);
+			//////////////////////////////////////////////////////////////////////////////////////////////////////////
+			/////////////////////////////////////////////   FILLSEED  ////////////////////////////////////////////////
+			//////////////////////////////////////////////////////////////////////////////////////////////////////////
+			REQUIRE(sHM.fillSeed(0,energy.size1()-1, 0,energy.size2()-1) == 0);
+
+
+			//////////////////////////////////////////////////////////////////////////////////////////////////////////
+			/////////////////////////////////////////////   TRACEBACK  ///////////////////////////////////////////////
+			//////////////////////////////////////////////////////////////////////////////////////////////////////////
+			size_t si1=9999999, si2=9999999;
+			while( sHM.updateToNextSeed(si1,si2,0,r1.size()-1,0,r2.size()) ) {
+				// prepare trace
+				Interaction i(r1,r2);
+				i.basePairs.push_back(energy.getBasePair(si1,si2));
+				i.basePairs.push_back(energy.getBasePair(si1+sHM.getSeedLength1(si1,si2)-1,si2+sHM.getSeedLength2(si1,si2)-1));
+				// trace base pairs
+				sHM.traceBackSeed(i, si1, si2);
+				// check number of traced base pairs
+				REQUIRE(i.basePairs.size() == bp);
+			}
+
 		}
 	}
 
@@ -122,8 +155,9 @@ TEST_CASE( "SeedHandlerMfe", "[SeedHandlerMfe]") {
 		ReverseAccessibility racc(acc2);
 		InteractionEnergyBasePair energy(acc1, racc);
 
+		size_t seedBP=4;
 		// seedBP / seedMaxUP / seedTMaxUP / seedQMaxUP / seedMaxE / seedMaxED / seedTRange / seedQRange / seedTQ
-		SeedConstraint sC(4,3,3,0,0
+		SeedConstraint sC(seedBP,3,3,0,0
 				, AccessibilityDisabled::ED_UPPER_BOUND
 				, 0
 				, IndexRangeList("")
@@ -138,5 +172,23 @@ TEST_CASE( "SeedHandlerMfe", "[SeedHandlerMfe]") {
 		/////////////////////////////////////////////   FILLSEED  ////////////////////////////////////////////////
 		//////////////////////////////////////////////////////////////////////////////////////////////////////////
 		REQUIRE(sHM.fillSeed(0,energy.size1()-1, 0,energy.size2()-1) == 2);
+
+
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////
+		/////////////////////////////////////////////   TRACEBACK  ///////////////////////////////////////////////
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////
+		size_t si1=9999999, si2=9999999;
+		while( sHM.updateToNextSeed(si1,si2,0,r1.size()-1,0,r2.size()) ) {
+			// prepare trace
+			Interaction i(r1,r2);
+			i.basePairs.push_back(energy.getBasePair(si1,si2));
+			i.basePairs.push_back(energy.getBasePair(si1+sHM.getSeedLength1(si1,si2)-1,si2+sHM.getSeedLength2(si1,si2)-1));
+			// trace base pairs
+			sHM.traceBackSeed(i, si1, si2);
+			// check number of traced base pairs
+			REQUIRE(i.basePairs.size() == seedBP);
+		}
 	}
+
+
 }
