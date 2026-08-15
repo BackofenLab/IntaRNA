56ac5eb2771db9c59235f21cc7b329a1660a87f8
AlexanderMitrofanov <mitrofan@informatik.uni-freiburg.de>
2026-08-15 06:49:26 +0200

    perf: remove redundant feasibility checks

--- src/IntaRNA/PredictorMfe2dSeed.cpp
+++ src/IntaRNA/PredictorMfe2dSeed.cpp
@@ -153,9 +153,7 @@ fillHybridE( const size_t j1, const size_t j2
 			hybridE_pq_seed(i1,i2) = E_INF;
 
 			// check if this cell is to be computed (!=E_INF)
-			if( energy.isAccessible1(i1)
-				&& energy.isAccessible2(i2)
-				&& energy.areComplementary(i1,i2))
+			if( energy.areComplementary(i1,i2))
 			{
 
 				// w2 = interaction width in seq2
@@ -184,9 +182,7 @@ fillHybridE( const size_t j1, const size_t j2
 					} else {
 						// no lp allowed
 						// check if right-side stacking of (i1,i2) is possible
-						if (energy.isAccessible1(i1+noLpShift)
-							&& energy.isAccessible2(i2+noLpShift)
-							&& energy.areComplementary(i1+noLpShift,i2+noLpShift))
+						if (energy.areComplementary(i1+noLpShift,i2+noLpShift))
 						{
 							// get stacking term to avoid recomputation
 							iStackE = energy.getE_interLeft(i1,i1+noLpShift,i2,i2+noLpShift);
